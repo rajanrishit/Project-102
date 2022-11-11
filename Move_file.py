12a204ce56281880e02df316491a1500f00b9e63
rajanrishit <87656072+rajanrishit@users.noreply.github.com>
2022-11-11 18:50:18 +0530

    Add files via upload

---
+++ Move_file.py
@@ -0,0 +1,35 @@
+from distutils import extension
+import os 
+import shutil
+
+from_dir="C:/Users/Hp/Downloads/P101"
+to_dir="C:/Users/Hp"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+    #print(name)
+    #print(extension)
+
+    if extension ==' ':
+        continue
+    if extension in ['.txt','.doc','.docx','.pdf']:
+        path1=from_dir + '/' + file_name
+        path2=to_dir + '/' + "C:/Users/Hp/Downloads/P101"
+        path3=to_dir + '/' + "C:/Users/Hp/Downloads/P101" + '/' + file_name
+
+    if os.path.exists(path2):
+        print("Moving " + file_name + ".....")   
+
+        #Moving from path1 to path3
+        shutil.move(path1 , path3)
+
+    else:
+        os.makedirs(path2)
+        print("Moving " + file_name + ".....")   
+
+        #Moving from path1 to path3
+        shutil.move(path1 , path3)
+
